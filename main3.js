c55072394c485e2ecb29c4c76ef407de9f36fa0c
Đamthanhtung <tungdtha140465@fpt.edu.vn>
2022-10-11 16:13:45 +0700

    do you love me

---
+++ main3.js
@@ -0,0 +1,11 @@
+
+function clickYes () {
+    alert("Yes, you are right")
+}
+function mouseOver () {
+    var x = Math.round(Math.random() * window.innerWidth);
+    var y = Math.round(Math.random() * window.innerHeight);
+    document.getElementById("no").style.left = x + 'px';
+    document.getElementById("no").style.top = y + 'px';
+
+}
